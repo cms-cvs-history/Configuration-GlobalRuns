c4ab3516f8dd157472fa4ce3d833ae5c2a5877e4
cmsbuild <>
2009-03-02 17:03:46 +0000

    This commit was manufactured by cvs2git to create tag 'Configuration- GlobalRuns-CMSSW_3_1_0_pre9'.
    Sprout from master 2008-09-17 14:55:50 UTC Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch> '219'
    Cherrypick from master 2008-11-28 12:01:52 UTC Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch> 'Set maxEvents to 100, consistent with recoT0DQM_EvContent_38T_cfg.py (And':
        python/recoT0DQM_EvContent_cfg.py
        python/recoT0DQM_EvContent_wAlca_cfg.py
    Cherrypick from master 2009-03-02 17:03:35 UTC David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch> 'cleanup 31x version':
        python/recoT0DQM_EvContent_38T_cfg.py
    Delete:
        data/DoubleMuon.cfg
        data/EventContentGR.cff
        data/ForceZeroTeslaField.cff
        data/MultiMuon.cfg
        data/PointingDoubleMultiSkim.cfg
        data/RawToDigiGR.cff
        data/SuperPointing.cfg
        data/TrackerPointing.cfg
        data/cosmic.cfg
        data/harvesting.cfg
        data/recoCosmicMC.cfg
        data/recoCosmicMCDQM.cfg
        data/recoDQMExample.cfg
        data/recoExampleGlobalTag.cfg
        data/recoMC_EvContent_0T.cfg
        data/recoT0-hcal.cfg
        data/recoT0.cfg
        data/recoT0DQM.cfg
        data/recoT0DQM_EvContent.cfg
        data/runDQM.cfg
        data/skimAlCaTk.cfg

--- python/recoT0DQM_EvContent_wAlca_cfg.py
+++ python/recoT0DQM_EvContent_wAlca_cfg.py
@@ -6,13 +6,11 @@ process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
 process.load("CondCore.DBCommon.CondDBSetup_cfi")
 
-process.maxEvents = cms.untracked.PSet(  input = cms.untracked.int32(100) )
+
+process.maxEvents = cms.untracked.PSet(  input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-# 'file:0C26943A-6D99-DD11-AE50-001617C3B76E.root'   
-      '/store/data/Commissioning08/Cosmics/RAW/v1/000/066/471/00682B3A-3F9B-DD11-81F9-000423D99A8E.root'
-#     '/store/data/Commissioning08/Cosmics/RAW/v1/000/065/682/201D431A-0699-DD11-AFE1-000423D98BE8.root'
-#     '/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/058/555/02E4041E-1571-DD11-98CE-001D09F241B9.root'
+     '/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/058/555/02E4041E-1571-DD11-98CE-001D09F241B9.root'
     )
 )
 
@@ -46,9 +44,50 @@ process.FEVT.outputCommands.append('keep PixelDigiedmDetSetVector_siPixelDigis_*
 process.FEVT.outputCommands.append('keep recoCandidatesOwned_caloTowersOpt_*_*')
 process.FEVT.outputCommands.append('keep RPCDetIdRPCDigiMuonDigiCollection_muonRPCDigis_*_*')
 
+# AlCaReco output modules
+process.ALCARECOStreamRpcCalHLT = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECORpcCalHLT')
+    ),
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep *_muonDTDigis_*_*', 
+        'keep CSCDetIdCSCWireDigiMuonDigiCollection_*_*_*', 
+        'keep CSCDetIdCSCStripDigiMuonDigiCollection_*_*_*', 
+        'keep DTLayerIdDTDigiMuonDigiCollection_*_*_*', 
+        'keep RPCDetIdRPCDigiMuonDigiCollection_*_*_*', 
+        'keep L1MuGMTCands_*_*_*', 
+        'keep L1MuGMTReadoutCollection_*_*_*'),
+    fileName = cms.untracked.string('ALCARECORpcCalHLT.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('StreamALCARECORpcCalHLT'),
+        dataTier = cms.untracked.string('ALCARECO')
+    )
+)
+process.ALCARECOStreamMuAlCalIsolatedMu = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECOMuAlCalIsolatedMu')
+    ),
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep *_ALCARECOMuAlCalIsolatedMu_*_*', 
+        'keep *_muonCSCDigis_*_*', 
+        'keep *_muonDTDigis_*_*', 
+        'keep *_muonRPCDigis_*_*', 
+        'keep *_dt1DRecHits_*_*', 
+        'keep *_dt2DSegments_*_*', 
+        'keep *_dt4DSegments_*_*', 
+        'keep *_csc2DRecHits_*_*', 
+        'keep *_cscSegments_*_*', 
+        'keep *_rpcRecHits_*_*'),
+    fileName = cms.untracked.string('ALCARECOMuAlCalIsolatedMu.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('StreamALCARECOMuAlCalIsolatedMu'),
+        dataTier = cms.untracked.string('ALCARECO')
+    )
+)
+
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.32 $'),
-    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GlobalRuns/python/recoT0DQM_EvContent_cfg.py,v $'),
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GlobalRuns/python/recoT0DQM_EvContent_wAlca_cfg.py,v $'),
     annotation = cms.untracked.string('CRUZET Prompt Reco with DQM with Mag field at 0T')
 )
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) ) ## default is false
@@ -72,6 +111,9 @@ process.load("Configuration.StandardSequences.RawToDigi_Data_cff")
 # reconstruction sequence for Cosmics
 process.load("Configuration.StandardSequences.ReconstructionCosmics_cff")
 
+# AlCaReco
+process.load('Configuration/StandardSequences/AlCaRecoStreams_cff')
+
 # offline DQM
 process.load("DQMOffline.Configuration.DQMOfflineCosmics_cff")
 process.load("DQMServices.Components.MEtoEDMConverter_cff")
@@ -81,8 +123,16 @@ process.load("DQMServices.Components.MEtoEDMConverter_cff")
 process.load("L1Trigger.Configuration.L1Config_cff")
 process.load("L1TriggerConfig.CSCTFConfigProducers.CSCTFConfigProducer_cfi")
 process.load("L1TriggerConfig.CSCTFConfigProducers.L1MuCSCTFConfigurationRcdSrc_cfi")
- 
-#Paths
-process.allPath = cms.Path( process.RawToDigi_woGCT * process.reconstructionCosmics *  process.DQMOfflineCosmics * process.MEtoEDMConverter)
 
+#Paths
+process.raw2digi_step = cms.Path(process.RawToDigi_woGCT)
+process.reconstruction_step = cms.Path(process.reconstructionCosmics)
+process.dqm_step = cms.Path(process.DQMOfflineCosmics * process.MEtoEDMConverter)
+process.pathALCARECORpcCalHLT = cms.Path(process.seqALCARECORpcCalHLT)
+process.pathALCARECOMuAlCalIsolatedMu = cms.Path(process.seqALCARECOMuAlCalIsolatedMu)
 process.outpath = cms.EndPath(process.FEVT)
+process.ALCARECOStreamRpcCalHLTOutPath = cms.EndPath(process.ALCARECOStreamRpcCalHLT)
+process.ALCARECOStreamMuAlCalIsolatedMuOutPath = cms.EndPath(process.ALCARECOStreamMuAlCalIsolatedMu)
+
+# Schedule definition
+process.schedule = cms.Schedule( process.raw2digi_step,process.reconstruction_step,process.pathALCARECORpcCalHLT,process.pathALCARECOMuAlCalIsolatedMu,process.dqm_step,process.outpath,process.ALCARECOStreamRpcCalHLTOutPath,process.ALCARECOStreamMuAlCalIsolatedMuOutPath)
